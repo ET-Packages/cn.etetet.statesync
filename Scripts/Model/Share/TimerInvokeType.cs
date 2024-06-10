892029dbe45bfa540ba589ab72a0f91ba7d497f2
tanghai <egametang@gmail.com>
2024-06-10 13:22:19 +0800

    init

---
+++ Scripts/Model/Share/TimerInvokeType.cs
@@ -0,0 +1,8 @@
+﻿namespace ET
+{
+    public static partial class TimerInvokeType
+    {
+        public const int SessionIdleChecker = PackageType.StateSync * 1000 + 1;
+        public const int SessionAcceptTimeout = PackageType.StateSync * 1000 + 2;
+    }
+}
